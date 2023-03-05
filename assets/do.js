2362710e936610e474d05443bea36a98609fcc38
Lucas Thevenard <lthevenard@gmail.br>
2023-03-05 16:26:06 -0300

    first commit

---
+++ assets/do.js
@@ -0,0 +1,9 @@
+function toggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
+
